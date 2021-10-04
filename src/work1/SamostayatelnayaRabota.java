7b11460ccb7625217c2b52878a122819754b272b
kamila <kamilasalehova11@gmail.com>
2021-10-04 09:50:15 -0400

    Ice cream added

--- src/work1/SamostayatelnayaRabota.java
+++ src/work1/SamostayatelnayaRabota.java
@@ -5,5 +5,6 @@ public class SamostayatelnayaRabota {
         System.out.println("Hello");
         System.out.println("Carrot");
         System.out.println("Cabbage");
+        System.out.println("Ice cream");
     }
 }
