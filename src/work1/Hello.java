c86e5dadc2dd4aabf218e75d34aea572ed3fe9e8
kamila <kamilasalehova11@gmail.com>
2021-10-03 23:35:33 -0400

    Added more work = third commit

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work 1 started");
         System.out.println("Created GIT repository");
+        System.out.println("Made few commits");
     }
 }
