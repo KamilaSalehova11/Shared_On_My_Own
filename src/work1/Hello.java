877632732b5987aadbefaf4c77104cf5fe53dbd0
kamila <kamilasalehova11@gmail.com>
2021-10-04 08:02:19 -0400

    Updating file to reshare on GitHub

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -7,5 +7,7 @@ public class Hello {
         System.out.println("Created GIT repository");
         System.out.println("Made few commits");
         System.out.println("Now its time to send to GitHub");
+        System.out.println("Do some changes in code");
+        System.out.println("Now it's ready to share on GitHUb");
     }
 }
