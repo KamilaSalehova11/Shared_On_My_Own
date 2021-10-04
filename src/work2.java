13b99bc9d5356af45357c680257d7313dff3319c
kamila <kamilasalehova11@gmail.com>
2021-10-03 23:47:22 -0400

    Updated file and second commit

--- src/work2.java
+++ src/work2.java
@@ -2,6 +2,7 @@ public class work2 {
     public static void main(String[] args) {
         System.out.println("Created work2 file");
         System.out.println("Updated file and first commit");
+        System.out.println("Updated file and second second commit");
 
     }
 }
