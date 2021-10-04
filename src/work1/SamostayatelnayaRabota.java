9777de50da7b74a55c9ee93157f6bd2cabf855da
kamila <kamilasalehova11@gmail.com>
2021-10-04 09:49:43 -0400

    Cabbage added

--- src/work1/SamostayatelnayaRabota.java
+++ src/work1/SamostayatelnayaRabota.java
@@ -4,5 +4,6 @@ public class SamostayatelnayaRabota {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Carrot");
+        System.out.println("Cabbage");
     }
 }
